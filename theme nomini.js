c3bf0997d534da70aee8d04a385417cf9f93c08e
jayhoa <hoadx@pstworld.com>
2021-12-13 01:10:45 +0700

    ss

--- theme nomini.js
+++ theme nomini.js
@@ -1,13 +1,7 @@
 function testAutoMinifi(arg) {
     var num = 1;
     var newNum = 2;
-<<<<<<< HEAD
     //test commands
     // var num2 = 2;s
-    // var num3 = 3;w
-=======
-    //test command
-    // var num2 = 2;ss
-    // var num3 = 3;csdsffss
->>>>>>> main
+    // var num3 = 3;ws
 }
